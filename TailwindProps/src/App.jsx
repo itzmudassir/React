9c798754a1c95e469931e53e73c58c7f7d25ac4a
itzmudassir <itzmudassir07@gmail.com>
2024-03-04 13:30:14 +0500

    Third Project: Tailwind CSS and Props

---
+++ TailwindProps/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Card btnText="Visit me" heading="About Macbook" />
+    <Card />
+    </>
+  )
+}
+
+export default App
