0a5b1bf3b487db02415cce0dbdd5f0db7df6080f
itzmudassir <itzmudassir07@gmail.com>
2024-03-06 19:32:06 +0500

    Few Changes

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("#05fad1");
+  const [color, setColor] = useState("#00ff9d");
   return (
     <>
       <div className="w-full h-screen duration-200"
       style={{backgroundColor: color}}>
+        <h1 className="text-2xl font-bold font-mono
+         text-black rounded-full
+          bg-white fixed m-2  px-2">Background Changer</h1>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
             <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
